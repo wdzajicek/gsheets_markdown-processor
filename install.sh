b6dd285fc7c3ab5092bf5186d64e3e7e90520478
wdzajicek <tig.welder4@gmail.com>
2019-07-03 11:00:51 -0500

    Add some NPMs!!

---
+++ install.sh
@@ -0,0 +1,57 @@
+
+echo "Creating local Gemfile from Gemfile.txt" && echo ""
+
+sleep 1
+
+# Create local Gemfile from Gemfile.txt
+cp Gemfile.txt Gemfile
+
+echo "" && echo "Running npm install" && echo ""
+
+sleep .5
+
+echo "" && echo "NPM will return permission errors if you do not have NVM - https://github.com/creationix/nvm" && echo ""
+
+sleep 1
+
+# NPM install
+npm install
+
+echo "" && echo "Running bundle install" && echo ""
+
+sleep 1
+
+# Bundle install
+bundle install
+
+sleep 1
+
+uname=$(uname)
+# If your on a Linux machine do...
+if [ "$uname" = "Linux" ]
+  then
+    # fix for node.js watch ENOSPC errors
+    # increases the max number of system watchers on linux
+    echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+    echo "" && echo "[LINUX]: Increase max watchers on Linux platforms" && echo ""
+    sleep 1
+  else
+    echo "" && echo "[Skipping](optional): Increase max watchers on Linux platforms. Your system: $uname" && echo ""
+fi
+unset UNAME
+
+sleep 1
+
+echo "" && echo "Removing kcc-startup-template's remote origin" && echo ""
+
+git remote remove origin
+
+sleep 1
+
+echo "" && echo "Removing kcc-startup-template's git history" && echo ""
+
+git init
+
+sleep 1
+
+echo "" && echo "======== DONE ========" && echo "" && echo "Installation Complete!" && echo "" && echo "======================"
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+process.traceDeprecation = true;
+const path = require('path');
+const HashPlugin = require('hash-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const config = {
+  mode: 'production',
+  watch: true,
+  //entry: path.join(__dirname, 'webpack', 'main'),
+  entry: {
+    'main': './assets/js/script/all.js',
+    'slim': './assets/js/slim/slim.js'
+  },
+  output: {
+    filename: '[name].[hash].bundle.js',
+    path: path.resolve(__dirname, 'assets', 'js', 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [
+          path.resolve(__dirname, 'node_modules')
+        ],
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HashPlugin({ path: './_data/', fileName: 'hash.yml' }),
+    new CleanWebpackPlugin({ path: './assets/js/dist/' })
+  ],
+  resolve: {
+    extensions: ['.json', '.js', '.jsx']
+  }
+};
+
+module.exports = config;
