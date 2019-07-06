923829ac4f488d2b57e053cf487cf51d43ec7611
wdzajicek <tig.welder4@gmail.com>
2019-07-06 16:36:05 -0500

    Bring gsheets/auth JS into bundle.

--- assets/js/script/all.js
+++ assets/js/script/all.js
@@ -1,8 +1,12 @@
 import lazyLoad from './lazyLoad.js';
 import footerDate from './footerDate.js';
+import handleClientLoad from './gapiInit.js';
+//import start from './sheetsAPI.js';
 import test from './test.js';
 
 document.addEventListener('DOMContentLoaded', function() {
   lazyLoad();
   footerDate();
+  //gapi.load('client', start);
+  handleClientLoad();
 });
